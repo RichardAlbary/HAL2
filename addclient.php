f898a0c78eae40a6ecc895d6fa744f89a59ee481
Richard Albary <r.albary@hotmail.com>
2020-07-22 16:27:20 +0100

    Update addclient.php

--- addclient.php
+++ addclient.php
@@ -1,5 +1,5 @@
 <?php
-
+// Load necessary libraries
 define('_JEXEC', 1);
 define('JPATH_BASE', __DIR__);  
 require_once JPATH_BASE . '/includes/defines.php';
@@ -10,6 +10,7 @@ $mainframe = JFactory::getApplication('site');
     ini_set('display_errors', '1');
     ini_set('error_reporting', E_ALL);
 
+// Initiate variables, assign values from POST data
 $firstName = $_POST['firstName'];
 $surname = $_POST['surname'];
 $emailaddress = $_POST['emailaddress'];
@@ -23,10 +24,11 @@ $phoneNumber1 = $_POST['phoneNumber1'];
 $phoneNumber2 = $_POST['phoneNumber2'];
 $otherPayee1 = $_POST['otherPayee1'];
 $otherPayee2 = $_POST['otherPayee2'];
-// Create an object for the record we are going to update.
+
+// Create an object for the record to be updated.
 $object = new stdClass();
 
-// Must be a valid primary key value.
+// Bind variable of POST data to object.
 $object->ClientName = $firstName;
 $object->ClientSurname = $surname;
 $object->ClientEmailAddress = $emailaddress;
@@ -41,9 +43,13 @@ $object->PhoneNumber2 = $phoneNumber2;
 $object->OtherPayeeName1 = $otherPayee1;
 $object->OtherPayeeName2 = $otherPayee2;
 
-// Update their details in the users table using id as the primary key.
+// Post client details in the client table.
 $result = JFactory::getDbo()->insertObject('o7ot5_client', $object, 'ClientNumber');
+
+// Obtain the client number of the newly created entry.
 $clientNumber = $object->ClientNumber;
+
+// Redirect site user to newly created client page
 header('Location: /clients/client-detail?'.$clientNumber);
 
 ?>
